62a9578a7ae4e6e7a9f24615b810e4d4b19d7580
Akira Ohgaki <akiraohgaki@gmail.com>
2023-04-10 02:45:47 +0900

    Update ecs-login.sh

--- ecs-login.sh
+++ ecs-login.sh
@@ -11,7 +11,7 @@ COMMAND='podman'
 WORK_DIR="$(cd "$(dirname "${0}")" && pwd)"
 
 if [ "${1}" = '--build' ]; then
-  COMMAND build -t localhost/aws-cli-with-session-manager-plugin:latest \
+  ${COMMAND} build -t localhost/aws-cli-with-session-manager-plugin:latest \
     -f "${WORK_DIR}/Containerfile" "${WORK_DIR}"
 fi
 
@@ -19,7 +19,7 @@ if [ ! -d "${WORK_DIR}/.aws" ]; then
   mkdir "${WORK_DIR}/.aws"
 fi
 
-COMMAND run --rm -ti \
+${COMMAND} run --rm -ti \
   -e AWS_ACCESS_KEY_ID="${ACCESS_KEY_ID}" \
   -e AWS_SECRET_ACCESS_KEY="${SECRET_ACCESS_KEY}" \
   -e AWS_DEFAULT_REGION="${REGION}" \

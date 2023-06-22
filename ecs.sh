df0be22765713398a9940fa7f123c55dea4abc47
Akira Ohgaki <akiraohgaki@gmail.com>
2023-06-23 01:58:04 +0900

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -7,29 +7,43 @@ CLUSTER=''
 TASK=''
 CONTAINER=''
 
-COMMAND='podman'
 WORK_DIR="$(cd "$(dirname "${0}")" && pwd)"
+IMAGE_BUILD_CONTEXT="${WORK_DIR}"
+IMAGE_TAG='localhost/aws-cli-with-session-manager-plugin:latest'
+
+COMMAND=''
+
+if [ "${COMMAND}" = '' ]; then
+  if [ "$(which docker)" ]; then
+    COMMAND='docker'
+  elif [ "$(which podman)" ]; then
+    COMMAND='podman'
+  else
+    echo 'docker or podman required'
+    exit 1
+  fi
+fi
 
 if [ "${1}" = '--build' ]; then
-  ${COMMAND} build -t localhost/aws-cli-with-session-manager-plugin:latest \
-    -f "${WORK_DIR}/Containerfile" "${WORK_DIR}"
+  "${COMMAND}" build -t "${IMAGE_TAG}" \
+    -f "${IMAGE_BUILD_CONTEXT}/Containerfile" "${IMAGE_BUILD_CONTEXT}"
 fi
 
 if [ ! -d "${WORK_DIR}/.aws" ]; then
   mkdir "${WORK_DIR}/.aws"
 fi
 
-${COMMAND} run --rm -ti \
+"${COMMAND}" run --rm -ti \
   -e AWS_ACCESS_KEY_ID="${ACCESS_KEY_ID}" \
   -e AWS_SECRET_ACCESS_KEY="${SECRET_ACCESS_KEY}" \
   -e AWS_DEFAULT_REGION="${REGION}" \
   -e AWS_DEFAULT_OUTPUT=json \
   -v "${WORK_DIR}/.aws:/root/.aws:z" \
   -v "${WORK_DIR}:/aws:z" \
-  localhost/aws-cli-with-session-manager-plugin:latest ecs execute-command \
+  "${IMAGE_TAG}" ecs execute-command \
   --region "${REGION}" \
   --cluster "${CLUSTER}" \
   --task "${TASK}" \
   --container "${CONTAINER}" \
   --interactive \
-  --command "/bin/bash"
+  --command '/bin/bash'

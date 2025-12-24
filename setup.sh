62c835426df3cc7faca5e55944533fdaa5a6f0ac
Akira Ohgaki <akiraohgaki@gmail.com>
2025-12-24 13:50:22 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if [ "${ARCH}" = 'aarch64' ] || [ "${ARCH}" = 'arm64' ]; then
   SESSION_MANAGER_PLUGIN_RPM_URL='https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_arm64/session-manager-plugin.rpm'
 fi
 
-curl -fsSL "${SESSION_MANAGER_PLUGIN_RPM_URL}" -o /tmp/session-manager-plugin.rpm
+curl -fsSL -o /tmp/session-manager-plugin.rpm "${SESSION_MANAGER_PLUGIN_RPM_URL}"
 
 yum install -y /tmp/session-manager-plugin.rpm
 
